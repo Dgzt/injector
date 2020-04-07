659365d40601fc253476450571ac9e556683b314
Zsuró Tibor <zsurotibor@gmail.com>
2020-04-07 17:21:17 +0200

    Gradle project. 1.0.0-SNAPSHOT version of Injector

---
+++ injector/src/com/dgzt/injector/Injector.java
@@ -0,0 +1,136 @@
+package com.dgzt.injector;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.ObjectMap;
+import com.badlogic.gdx.utils.reflect.ClassReflection;
+import com.badlogic.gdx.utils.reflect.Constructor;
+import com.badlogic.gdx.utils.reflect.ReflectionException;
+import com.dgzt.injector.exception.NewInstanceCreateException;
+
+/**
+ * The injector for runtime inject.
+ */
+public class Injector {
+
+    // --------------------------------------------------
+    // ~ Private members
+    // --------------------------------------------------
+
+    /**
+     * The instance object
+     */
+    private static Injector instance;
+
+    /**
+     * The object holder map
+     */
+    private final ObjectMap<Class<?>, Object> objects;
+
+    // --------------------------------------------------
+    // ~ Constructors
+    // --------------------------------------------------
+
+    /**
+     * The hidden constructor.
+     */
+    private Injector() {
+        objects = new ObjectMap<Class<?>, Object>();
+    }
+
+    // --------------------------------------------------
+    // ~ Public methods
+    // --------------------------------------------------
+
+    /**
+     * @param clazz The class of object.
+     * @param <T>   The type of object.
+     * @return The object.
+     */
+    public static <T> T get(final Class<T> clazz) {
+        createInstanceIfNecessary();
+
+        if (!isInitialized(clazz)) {
+            initialize(clazz);
+        }
+
+        return clazz.cast(instance.objects.get(clazz));
+    }
+
+    /**
+     * @param clazz The checking class.
+     * @param <T>   The type of class.
+     * @return True if class has initialized otherwise false.
+     */
+    public static <T> boolean isInitialized(final Class<T> clazz) {
+        createInstanceIfNecessary();
+
+        return instance.objects.containsKey(clazz);
+    }
+
+    /**
+     * Initializes the object.
+     *
+     * @param clazz The class.
+     * @param <T>   The type of class
+     */
+    public static <T> void initialize(final Class<T> clazz) {
+        createInstanceIfNecessary();
+
+        try {
+            final Constructor[] constructors = ClassReflection.getConstructors(clazz);
+            final Constructor constructor = constructors[0];
+            final Class<?>[] parameterTypes = constructor.getParameterTypes();
+
+            final Object[] parameters = new Object[parameterTypes.length];
+            for (int i = 0; i < parameterTypes.length; ++i) {
+                parameters[i] = get(parameterTypes[i]);
+            }
+
+            instance.objects.put(clazz, constructor.newInstance(parameters));
+        } catch (final ReflectionException e) {
+            Gdx.app.error("", "Could not create new instance! ", e);
+            throw new NewInstanceCreateException(e);
+        }
+    }
+
+    /**
+     * Initializes the class with the given object.
+     *
+     * @param clazz  The class of object.
+     * @param object The object.
+     * @param <T>    The type of class.
+     */
+    public static <T> void initialize(final Class<T> clazz, final T object) {
+        createInstanceIfNecessary();
+
+        instance.objects.put(clazz, object);
+    }
+
+    /**
+     * Disposes the objects what implements {@link Disposable} interface
+     * and clears the object map.
+     */
+    public static void dispose() {
+        if (instance != null) {
+            for (final ObjectMap.Entry<Class<?>, Object> entry : instance.objects.entries()) {
+                if (entry.value instanceof Disposable) {
+                    ((Disposable) entry.value).dispose();
+                }
+            }
+
+            instance.objects.clear();
+        }
+    }
+
+    // --------------------------------------------------
+    // ~ Private methods
+    // --------------------------------------------------
+
+    private static void createInstanceIfNecessary() {
+        if (instance == null) {
+            instance = new Injector();
+        }
+    }
+
+}
---
+++ injector/src/com/dgzt/injector/exception/NewInstanceCreateException.java
@@ -0,0 +1,26 @@
+package com.dgzt.injector.exception;
+
+/**
+ * The exception if the injector cannot create new instance.
+ */
+public class NewInstanceCreateException extends RuntimeException {
+
+    // --------------------------------------------------
+    // ~ Constants
+    // --------------------------------------------------
+
+    private static final long serialVersionUID = 1L;
+
+    // --------------------------------------------------
+    // ~ Constructors
+    // --------------------------------------------------
+
+    /**
+     * The constructor.
+     *
+     * @param cause The cause exception
+     */
+    public NewInstanceCreateException(final Throwable cause) {
+        super(cause);
+    }
+}
